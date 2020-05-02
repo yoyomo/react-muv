13f3d08b6f1bd93987bedf52145d0bfecc5bd01e
Armando <armando.josue.og@gmail.com>
2020-05-01 21:55:06 -0700

    Root inside core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Root from "./Root";
+import Root from "./core";
 import Model from './model';
 import Update from './update';
 import View from './view';
